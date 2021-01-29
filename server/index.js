831b67fb256d93c665582a810aa4133f92cc5a24
Christina Santos <christina.santos823@gmail.com>
2021-01-29 12:01:01 -0800

    Updated styles and added image patch request

--- server/index.js
+++ server/index.js
@@ -3,24 +3,29 @@ const axios = require('axios');
 const app = require('./server.js');
 
 app.get('/api/relatedItems/:itemID', (req, res) => {
-    axios.get(`http://3.23.86.72:3001/api/relatedItems/${5}`)
+    axios.get(`http://3.23.86.72:3001/api/relatedItems/${0}`)
     .then((result) => {res.send(result.data)});
 });
 
 app.get('/api/reviews/:id', (req, res) => {
-    axios.get(`http://18.221.226.62:3002/api/reviews/${5}`)
+    axios.get(`http://18.221.226.62:3002/api/reviews/${0}`)
     .then((result) => res.send(result.data));
 });
 
 app.get('/api/item/:itemID', (req, res) => {
-    axios.get(`http://3.140.252.86:3003/api/item/${5}`)
+    axios.get(`http://3.140.252.86:3003/api/item/${0}`)
     .then((result) => res.send(result.data));
 });
 
 app.get('/api/items/:itemID', (req, res) => {
-    axios.get(`http://3.140.248.238:3004/api/items/${5}`)
+    axios.get(`http://3.140.248.238:3004/api/items/${0}`)
     .then((result) => res.send(result.data));
 });
+
+app.patch('/api/items/:itemID', (req, res) => {
+    axios.patch(`http://3.140.248.238:3004/api/items/${0}`, { favorite: req.body.favorite })
+      .then(() => res.sendStatus(204));
+  });
 
 const PORT = 3000|| process.env.PORT;
 
