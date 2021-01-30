5f18f16d81413d99227da65ac2a953eafdd0affe
Christina Santos <christina.santos823@gmail.com>
2021-01-30 12:28:33 -0800

    Updated imgs to S3, adjusted CSS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,38 @@
 const server = require('./server.js');
 const axios = require('axios'); 
 const app = require('./server.js');
+const AWS = require('aws-sdk');
+const path = require('path');
+const fs = require('fs');
+
+AWS.config.loadFromPath(path.resolve(__dirname, 'config.json'));
+AWS.config.update({
+    accessKeyId: AWS.config.credentials.accessKeyId,
+    secretAccessKey: AWS.config.credentials.secretAccessKey,
+    region: AWS.config.region
+});
+
+const s3 = new AWS.S3();
+var params;
+var bucketParams = {
+    Bucket: 'croxy-proxy-imgs',
+};
+
+s3.listObjects(bucketParams, function(err, data) {
+    if(err) {
+        console.log("Error", err);
+    } else {
+        data.Contents.map((img) => {
+            params = {
+                Bucket: 'croxy-proxy-imgs',
+                Key: img.Key
+            };
+        let readStream = s3.getObject(params).createReadStream();
+        let writeStream = fs.createWriteStream(path.join('public/imgs', img.Key));
+        readStream.pipe(writeStream);
+        })
+    }
+});
 
 app.get('/api/relatedItems/:itemID', (req, res) => {
     axios.get(`http://3.23.86.72:3001/api/relatedItems/${0}`)
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static(PUBLIC_DIR));
 
 module.exports = app;
